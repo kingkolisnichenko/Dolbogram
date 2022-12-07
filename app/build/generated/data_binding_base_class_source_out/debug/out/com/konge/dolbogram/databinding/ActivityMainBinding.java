7f344b7bee735bbc8c72ab90f74966997505b94a
Ilya <ilya08.10.98@gmail.com>
2022-12-07 15:44:42 +0200

    2

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/konge/dolbogram/databinding/ActivityMainBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/konge/dolbogram/databinding/ActivityMainBinding.java
@@ -6,18 +6,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.konge.dolbogram.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class ActivityMainBinding implements ViewBinding {
   @NonNull
   private final ConstraintLayout rootView;
 
-  private ActivityMainBinding(@NonNull ConstraintLayout rootView) {
+  @NonNull
+  public final Toolbar mainToolbar;
+
+  private ActivityMainBinding(@NonNull ConstraintLayout rootView, @NonNull Toolbar mainToolbar) {
     this.rootView = rootView;
+    this.mainToolbar = mainToolbar;
   }
 
   @Override
@@ -43,10 +50,19 @@ public final class ActivityMainBinding implements ViewBinding {
 
   @NonNull
   public static ActivityMainBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.mainToolbar;
+      Toolbar mainToolbar = ViewBindings.findChildViewById(rootView, id);
+      if (mainToolbar == null) {
+        break missingId;
+      }
 
-    return new ActivityMainBinding((ConstraintLayout) rootView);
+      return new ActivityMainBinding((ConstraintLayout) rootView, mainToolbar);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
