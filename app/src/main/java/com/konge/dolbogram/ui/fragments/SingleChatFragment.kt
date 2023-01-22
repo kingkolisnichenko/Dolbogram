cc4e68999380f1e7be45e4a117f72669c08559e1
Ilya <ilya08.10.98@gmail.com>
2023-01-22 17:17:37 +0200

    v.0.34.1

---
+++ app/src/main/java/com/konge/dolbogram/ui/fragments/SingleChatFragment.kt
@@ -0,0 +1,10 @@
+package com.konge.dolbogram.ui.fragments
+
+import com.konge.dolbogram.R
+
+class SingleChatFragment : BaseFragment(R.layout.fragment_single_chat) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
